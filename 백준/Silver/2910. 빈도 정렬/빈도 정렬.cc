3ee0655e1a6e2e2ef317290318e41982e7aed4c4
matcha-bingsu <76214202+matcha-bingsu@users.noreply.github.com>
2022-09-26 02:10:52 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, number, m;
-unordered_map<int, int> mp; 
-unordered_map<int, int> mp2;
-vector<pair<int, int> > v;     
+int n, c, num;
+map<int, int>   m, order;
 
-bool    compare(pair<int, int> a, pair<int, int> b)
+bool    cmp(pair<int, int> &a, pair<int, int> &b)
 {
     if (a.second == b.second)
-        return (mp2[a.first] < mp2[b.first]);
+        return (order[a.first] < order[b.first]);
     return (a.second > b.second);
 }
 
@@ -17,23 +15,18 @@ int main(void)
     cin.tie(0);
     ios::sync_with_stdio(0);
 
-    cin >> n >> m;
+    cin >> n >> c;
     for (int i = 0; i < n; i++)
     {
-        cin >> number;
-        if (mp.find(number) == mp.end())
-        {
-           mp.insert({number, 1});
-           mp2.insert({number, i});
-        }
-        else
-           mp[number]++;
+        cin >> num;
+        m[num]++;
+        if (order[num] == 0)
+            order[num] = i + 1; 
     }
-    for (auto c : mp)
-        v.push_back({c.first, c.second});
-    sort(v.begin(), v.end(), compare);
-    for (auto c : v)
-        for (int i = 0; i < c.second; i++)
-            cout << c.first << ' ';
+    vector<pair<int, int>> vec(m.begin(), m.end()); 
+    sort(vec.begin(), vec.end(), cmp);
+    for (auto a : vec)
+        for (int i = 0; i < a.second; i++)
+            cout << a.first << ' ';
     return (0);
 }
