acd6343d6507b36e812f5395ff5260c70e619078
matcha-bingsu <76214202+matcha-bingsu@users.noreply.github.com>
2022-11-08 16:59:53 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 1,283 ms, Memory: 52,728 KB -BaekjoonHub

---
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[4][4];
+int chck[10000000];
+int dx[4] = {0, -1, 0, 1};
+int dy[4] = {-1, 0, 1, 0};
+
+void    init()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+            cin >> arr[i][j];
+    }
+    fill(chck, chck + 10000000, 0);
+}
+
+void    bfs(int i, int j)
+{
+    queue<pair<pair<int, int>, pair<int, int>>>  q;
+    q.push({{i, j}, {1, arr[i][j]}});
+    while (!q.empty())
+    {
+        int x, y, len, number;
+
+        tie(x, y) = q.front().first;
+        tie(len, number) = q.front().second;
+        q.pop();
+        if (len == 7)
+        {
+            chck[number] = 1;
+            continue;
+        }
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int new_x = x + dx[dir];
+            int new_y = y + dy[dir];
+            if (new_x < 0 || new_y < 0 || new_x >= 4 || new_y >= 4)
+                continue;
+            q.push({{new_x, new_y}, {len + 1, number * 10 + arr[new_x][new_y]}});
+        }
+    }
+}
+
+int main(void)
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int test_case;
+    
+    cin >> test_case;
+    for (int cur_case = 1; cur_case <= test_case; cur_case++)
+    {
+        init();
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+                bfs(i, j);
+        }
+        cout << '#' << cur_case << ' ' << accumulate(chck, chck + 10000000, 0) << '\n'; 
+    }
+    return (0);
+}
